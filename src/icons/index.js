57eb4b6f132508ae0c7ff141aa40fc7866358821
rongyueHu <1752333046@qq.com>
2022-07-13 17:21:41 +0800

    编辑登录页面

--- src/icons/index.js
+++ src/icons/index.js
@@ -3,7 +3,7 @@ import SvgIcon from '@/components/SvgIcon'// svg component
 
 // register globally
 Vue.component('svg-icon', SvgIcon)
-
+// 批量导入.svg，false第一层
 const req = require.context('./svg', false, /\.svg$/)
 const requireAll = requireContext => requireContext.keys().map(requireContext)
 requireAll(req)
--- vue.config.js
+++ vue.config.js
@@ -35,6 +35,20 @@ module.exports = {
     overlay: {
       warnings: false,
       errors: true
+    },
+    // 反向代理
+    proxy: {
+      // 只要是axios发请求的时候，前面都是以/abc开头的，就会被这块匹配到，就会自动给我们做反向代理
+      // 所有的当前这个人资接口url都是以api开头的
+      '/abc': {
+        target: 'http://ihrm.itheima.net/',
+        changeOrigin: true,
+        pathRewrite: {
+          // 相当于是在做replace替换操作
+          // ^特☞开头
+          '^/abc': ''
+        }
+      }
     }
     // 因为我们真正的项目有自己接口服务器
     // before: require('./mock/mock-server.js')
